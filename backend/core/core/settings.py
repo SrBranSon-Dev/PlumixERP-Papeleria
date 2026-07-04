ab7cf53462c18b3971404459cec73424a3252df0
BranCode <bransoncode.dev@gmail.com>
2026-07-04 06:45:00 -0500

    DB creada, migrada y conectada

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -77,12 +77,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "mydb",
+        "USER": "root",
+        "PASSWORD": "Admin123456789",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
