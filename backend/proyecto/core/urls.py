3a27f02820a132334c929b10e7ba58d9615712cc
BranCode <bransoncode.dev@gmail.com>
2026-09-20 11:48:12 -0500

    Se creo y se probo el crud de clientes

--- backend/proyecto/core/urls.py
+++ backend/proyecto/core/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
 
     path("api/", include("usuarios.urls")),
     path("api/", include("proveedores.urls")),
+    path("api/", include("clientes.urls")),
     path("api/productos/", include("productos.urls")),
 
     path("api/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
