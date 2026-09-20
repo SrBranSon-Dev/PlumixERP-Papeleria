4e6b4c641879ea37112245d081ea167aaed73bcb
BranCode <bransoncode.dev@gmail.com>
2026-09-20 11:22:51 -0500

    Se crearon las migraciones de clientes

--- backend/proyecto/clientes/models.py
+++ backend/proyecto/clientes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    correo_electronico = models.EmailField(unique=True)
+    telefono = models.CharField(max_length=15, blank=True, null=True)
+    direccion = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
